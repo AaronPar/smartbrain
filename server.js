cea1ac6d1972e7b07a964df48de43f8f46df51d9
AaronPar <20910737Par>
2019-03-15 11:56:54 +0100

    comment

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/Image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-reticulated-67299',
       user : 'postgres',
       password : '20910737par',
       database : 'smartbrain'
